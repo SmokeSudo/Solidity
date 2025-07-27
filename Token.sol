efd05e38d0de433ddb3a5e34b605d5df6d6c8aa9
Smoke Sudo <rebelonthabeat@gmail.com>
2025-07-28 00:44:18 +0100

    Add files via upload

---
+++ Token.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract BIGFEMSToken {
+    string public name = "BIGFEMS";
+    string public symbol = "FEMS";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    address public owner;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor() {
+        owner = msg.sender;
+        totalSupply = 777_777_777 * (10 ** uint256(decimals));
+        balanceOf[owner] = totalSupply;
+        emit Transfer(address(0), owner, totalSupply);
+    }
+
+    function transfer(address to, uint256 amount) public returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+}
